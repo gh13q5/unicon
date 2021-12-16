f1629e54a4c4cbae56ac659be0662be97b0c63de
Juhee Park <58504730+Juhee-Park@users.noreply.github.com>
2021-12-16 23:21:57 +0900

    Update UserRegisterController.java

--- src/main/java/controller/info/UserRegisterController.java
+++ src/main/java/controller/info/UserRegisterController.java
@@ -12,50 +12,52 @@ import org.slf4j.LoggerFactory;
 import controller.Controller;
 import model.User;
 import model.dao.UserDAO;
+import model.Interests;
+import model.dao.InterestsDAO;
 
 import model.service.ExistingUserException;
 
-
 public class UserRegisterController implements Controller {
-    private static final Logger log = LoggerFactory.getLogger(UserRegisterController.class);
+	private static final Logger log = LoggerFactory.getLogger(UserRegisterController.class);
 
-    @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-     //받은 날짜를 date로 변환하는 함수
-       	SimpleDateFormat userDate = new SimpleDateFormat("yyyy-MM-dd");
-       	
-     // POST request (회원정보가 parameter로 전송됨)
-       	User user = new User(
-			request.getParameter("userId"),
-			request.getParameter("password"),
-			request.getParameter("email"),
-			request.getParameter("name"),
-			request.getParameter("phone_number"),
-			userDate.parse(request.getParameter("birthDay")),
-			Integer.parseInt(request.getParameter("gender")),
-			0);
+		// 諛쏆� �궇吏쒕�� date濡� 蹂��솚�븯�뒗 �븿�닔
+		SimpleDateFormat userDate = new SimpleDateFormat("yyyy-MM-dd");
+
+		// POST request (�쉶�썝�젙蹂닿� parameter濡� �쟾�넚�맖)
+		User user = new User(Integer.parseInt(request.getParameter("gender")), request.getParameter("Id"),
+				request.getParameter("password"),
+				request.getParameter("email"), request.getParameter("name"), request.getParameter("phone_number"),
+				userDate.parse(request.getParameter("birthDay")), Integer.parseInt(request.getParameter("gender")), 0);
 		
-        log.debug("Create User : {}", user);
+
+		log.debug("Create User : {}", user);
 
 		try {
+			if(request.getParameter("password") != request.getParameter("passwordCheck")) {
+				return "redirect:/userRegisterForm.jsp";
+			}
 			create(user);
-	        return "/welcome.jsp";	// 성공 시 사용자 리스트 화면으로 redirect
-	        
-		} catch (ExistingUserException e) {	// 예외 발생 시 회원가입 form으로 forwarding
-            request.setAttribute("registerFailed", true);
+			return "redirect:/welcome.jsp"; // �꽦怨� �떆 �궗�슜�옄 由ъ뒪�듃 �솕硫댁쑝濡� redirect
+
+		} catch (ExistingUserException e) { // �삁�쇅 諛쒖깮 �떆 �쉶�썝媛��엯 form�쑝濡� forwarding
+			request.setAttribute("registerFailed", true);
 			request.setAttribute("exception", e);
 			request.setAttribute("user", user);
-			return "/userRegisterForm.jsp";
+			return "redirect:/userRegisterForm.jsp";
 		}
-    }
+	}
+
 	public int create(User user) throws SQLException, ExistingUserException {
-		
+
 		UserDAO userManager = new UserDAO();
-		
+
 		if (userManager.existingUser(user.getUserId()) == true) {
 			throw new ExistingUserException(user.getUserId() + "is aleady exist.");
 		}
 		return userManager.create(user);
 	}
+
 }
